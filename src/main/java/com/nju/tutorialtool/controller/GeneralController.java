1d71e38003bda74477c685ec97f05ca55c3bee69
bcy97 <609233559@qq.com>
2018-05-19 23:21:16 +0800

    add 完成rabbitmq部分

--- src/main/java/com/nju/tutorialtool/controller/GeneralController.java
+++ src/main/java/com/nju/tutorialtool/controller/GeneralController.java
@@ -1,6 +1,7 @@
 package com.nju.tutorialtool.controller;
 
 import com.nju.tutorialtool.service.HystrixService.AddHystrixService;
+import com.nju.tutorialtool.service.RabbitmqService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,22 +18,47 @@ public class GeneralController {
     @Autowired
     private AddHystrixService addHystrixService;
 
+    @Autowired
+    private RabbitmqService rabbitmqService;
+
+
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addGeneral(@RequestBody String url, boolean isEurekaServer,boolean isZuul,boolean isHystrix,boolean isRabbitMQ,boolean isRibbon){
-        if(isEurekaServer==true){
+    public void addGeneral(@RequestBody String url, boolean isEurekaServer, boolean isZuul, boolean isHystrix, boolean isRabbitMQ, boolean isRibbon, String src, String dest, String mqPath) {
+        if (isEurekaServer == true) {
 
         }
-        if(isZuul==true){
+        if (isZuul == true) {
 
         }
-        if(isHystrix==true){
+        if (isHystrix == true) {
             addHystrixService.add(url);
         }
-        if(isRabbitMQ==true){
+        if (isRabbitMQ == true) {
+            addRabbitmq(mqPath);
+            addSender(src);
+            addReceiver(dest);
+        }
+        if (isRibbon == true) {
 
         }
-        if(isRibbon==true){
+    }
 
-        }
+
+    public void addRabbitmq(String location) {
+        String path = rabbitmqService.find(location);
+        String direct = path.substring(location.length() + 15);
+        rabbitmqService.addQueue(path, direct);
+    }
+
+    public void addSender(String location) {
+        String path = rabbitmqService.find(location);
+        String direct = path.substring(location.length() + 15);
+        rabbitmqService.addSender(path, direct);
+    }
+
+    public void addReceiver(String location) {
+        String path = rabbitmqService.find(location);
+        String direct = path.substring(location.length() + 15);
+        rabbitmqService.addRecevier(path, direct);
     }
 }
