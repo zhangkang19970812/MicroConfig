eaa2082befe4595609fa6cfb562ecee85ecc3628
YZ <1150681747@qq.com>
2018-05-20 16:38:06 +0800

    整合zuul和ribbon

--- src/main/java/com/nju/tutorialtool/controller/GeneralController.java
+++ src/main/java/com/nju/tutorialtool/controller/GeneralController.java
@@ -2,11 +2,16 @@ package com.nju.tutorialtool.controller;
 
 import com.nju.tutorialtool.service.HystrixService.AddHystrixService;
 import com.nju.tutorialtool.service.RabbitmqService;
+import com.nju.tutorialtool.service.RibbonService;
+import com.nju.tutorialtool.service.ZuulService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.util.List;
 
 /**
  * @Author YZ
@@ -17,29 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class GeneralController {
     @Autowired
     private AddHystrixService addHystrixService;
-
     @Autowired
     private RabbitmqService rabbitmqService;
-
+    @Autowired
+    private RibbonService ribbonService;
+    @Autowired
+    private ZuulService zuulService;
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
-    public void addGeneral(@RequestBody String url, boolean isEurekaServer, boolean isZuul, boolean isHystrix, boolean isRabbitMQ, boolean isRibbon, String src, String dest, String mqPath) {
+    public void addGeneral(@RequestBody String url, boolean isEurekaServer, boolean isZuul, boolean isHystrix, boolean isRabbitMQ, boolean isRibbon, String mqsrc, String mqdest, String mqPath,String zuulConsumerPath, List<String> zuulProviderPath) throws IOException {
         if (isEurekaServer == true) {
 
         }
         if (isZuul == true) {
-
+            zuulService.replaceUrl(zuulConsumerPath,zuulProviderPath);
         }
         if (isHystrix == true) {
             addHystrixService.add(url);
         }
         if (isRabbitMQ == true) {
             addRabbitmq(mqPath);
-            addSender(src);
-            addReceiver(dest);
+            addSender(mqsrc);
+            addReceiver(mqdest);
         }
         if (isRibbon == true) {
-
+            ribbonService.addRibbon(url);
         }
     }
 
