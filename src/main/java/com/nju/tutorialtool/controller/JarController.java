3e80a3160c52e926a0d9b12ae8b3f404f71578e0
张康 <131250005@smail.nju.edu.cn>
2018-05-23 01:26:02 +0800

    修改：将EurekaServerInfo改成SpringCloudInfo，因为Zuul也用到这个model，所以改一下 添加： Zuul Ribbon 为业务服务pom文件添加依赖，该方法在IO，可以调用 将项目打成jar包 为项目创建Dockerfile

--- src/main/java/com/nju/tutorialtool/controller/JarController.java
+++ src/main/java/com/nju/tutorialtool/controller/JarController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/jar")
 public class JarController {
+
     @Autowired
     private GenerateJarService generateJarService;
 
