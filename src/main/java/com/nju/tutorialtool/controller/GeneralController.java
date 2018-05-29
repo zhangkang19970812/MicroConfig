43ec4841c8c2446cace532b4c93600ebb8500680
Shenmiu <gcm971016@gmail.com>
2018-05-29 16:34:13 +0800

    fix(): 将上传文件api接口整合到GeneralController

--- src/main/java/com/nju/tutorialtool/controller/GeneralController.java
+++ src/main/java/com/nju/tutorialtool/controller/GeneralController.java
@@ -1,15 +1,17 @@
 package com.nju.tutorialtool.controller;
 
+import com.alibaba.fastjson.JSON;
 import com.nju.tutorialtool.model.*;
 import com.nju.tutorialtool.model.dto.RibbonDTO;
 import com.nju.tutorialtool.service.*;
 import com.nju.tutorialtool.service.HystrixService.AddHystrixService;
+import com.nju.tutorialtool.util.FileUtil;
 import com.nju.tutorialtool.util.enums.BaseDirConstant;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
@@ -48,7 +50,14 @@ public class GeneralController {
     @Autowired
     private CreateMysqlProjectService createMysqlProjectService;
 
-    private static final String UPLOAD_FOLDER = "upload";
+    private Logger logger = LoggerFactory.getLogger(GeneralController.class);
+
+    @PostMapping(value = "/uploadFolder")
+    public String uploadFolder(MultipartFile[] folder) {
+        logger.info("传入的文件参数：{}", JSON.toJSONString(folder, true));
+        FileUtil.saveMultiFile(BaseDirConstant.projectBaseDir, folder);
+        return "ok";
+    }
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public void addGeneral(@RequestBody General general) throws Exception {
