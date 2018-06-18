86ee4ff6c002c1dea879a4cedddddf17f7bd6e15
张康 <131250005@smail.nju.edu.cn>
2018-06-18 19:34:20 +0800

    user加Id注解

--- src/main/java/com/nju/tutorialtool/model/User.java
+++ src/main/java/com/nju/tutorialtool/model/User.java
@@ -1,9 +1,11 @@
 package com.nju.tutorialtool.model;
 
 import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+    @Id
     private int id;
 
     public User() {
