8471b5c0fd854d3e74b4a5433e809f5f56facef9
YZ <1150681747@qq.com>
2018-06-28 17:44:42 +0800

    修改main

--- src/main/java/com/nju/tutorialtool/service/HystrixService/Main.java
+++ src/main/java/com/nju/tutorialtool/service/HystrixService/Main.java
@@ -9,10 +9,10 @@ import java.io.IOException;
 public class Main {
     public static void main(String[]args){
         AddMethods ad=new AddMethods();
-        try {
-            ad.addMethods("/Users/YZ/Desktop/微服务/PetStore-/order_service");
-        } catch (IOException e) {
-            System.out.print("文件不存在！");
-        }
+//        try {
+//            ad.addMethods("/Users/YZ/Desktop/微服务/PetStore-/order_service");
+//        } catch (IOException e) {
+//            System.out.print("文件不存在！");
+//        }
     }
 }
