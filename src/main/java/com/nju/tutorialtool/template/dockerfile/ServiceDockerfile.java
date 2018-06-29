3cf7a15e1185f307570da66d120ab87a2c0751b0
张康 <131250005@smail.nju.edu.cn>
2018-06-29 18:44:00 +0800

    修改ServiceDockerfile中的jar包名称

--- src/main/java/com/nju/tutorialtool/template/dockerfile/ServiceDockerfile.java
+++ src/main/java/com/nju/tutorialtool/template/dockerfile/ServiceDockerfile.java
@@ -7,11 +7,11 @@ public class ServiceDockerfile extends DockerFile {
         super(fileDir);
         String projectName = getProjectName(fileDir);
         String instructions = "FROM ewolff/docker-java\r\n" +
-                "COPY target/"+ projectName +"-0.0.1-SNAPSHOT.jar .\r\n" +
+                "COPY target/msconfig.jar .\r\n" +
                 "COPY target/config/application.properties /config/application.properties\r\n" +
-                "RUN sh -c 'touch /"+ projectName +"-0.0.1-SNAPSHOT.jar'\r\n" +
+                "RUN sh -c 'touch /msconfig.jar'\r\n" +
                 "ENV JAVA_OPTS=\"\"\r\n" +
-                "ENTRYPOINT [ \"sh\", \"-c\", \"java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /"+ projectName +"-0.0.1-SNAPSHOT.jar\" ]\r\n" +
+                "ENTRYPOINT [ \"sh\", \"-c\", \"java $JAVA_OPTS -Djava.security.egd=file:/dev/./urandom -jar /msconfig.jar\" ]\r\n" +
                 "EXPOSE " + port;
         init(instructions);
     }
